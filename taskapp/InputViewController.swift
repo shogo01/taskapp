ab69db8934d227de144d7c85dac1f00e3d49eb8a
shogo <ujiiiiie@gmail.com>
2020-06-15 10:31:41 +0900

    レッスン６　5.4 InputViewControllerのパーツを設定する　まで終了

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by USER on 2020/06/15.
+//  Copyright © 2020 shogo.ujiie. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
